3089414b85c8fdc0517bb053cf2eb0cabaee1193
AlexVYard <alexvegayard@gmail.com>
2023-06-08 13:09:24 -0500

    added header and filters to kitchen
    added min-height to wallpapers
    added logout to office screen

--- src/modules/kitchen/index.jsx
+++ src/modules/kitchen/index.jsx
@@ -1,18 +1,18 @@
-import /* React,  */{ useState, useEffect } from 'react'
-import { /* Navigate,  */useNavigate } from "react-router-dom"
+import /* React,  */ { useState, useEffect } from 'react'
+// import { /* Navigate,  */useNavigate } from "react-router-dom"
 // import { postOrder } from '../scripts/postOrder';
 import { database } from '../../features/database';
 // import { useCheckAuth } from '../scripts/checkAuth';
+import { logout } from '../../features/logout';
 
 function Kitchen() {
 
   let [orders, setOrders] = useState()
+  let [filter, setFilter] = useState('')
   // const [order, orderReady] = useState([])
   // const [readyButtonText, setReadyButtonText] = useState('Listo')
   // const [noText, setNoText] = useState(true)
   // const [readyButton, setReadyButton] = useState(false)
-
-  const navigate = useNavigate();
 
   const readyButtonText = (status) => {
     if (status === 'pending') {
@@ -20,7 +20,7 @@ function Kitchen() {
     }
     if (status === 'processed') {
       return 'Entregado'
-    }    
+    }
     if (status === 'delivered') {
       return 'Comido'
     }
@@ -39,14 +39,15 @@ function Kitchen() {
   useEffect(() => {  // getting info from database
     // fetch data
     const ordersFetch = async () => {
-      const orders = await database('orders', 'GET', localStorage.getItem("accessToken"))
+      let orders = await database('orders', 'GET', localStorage.getItem("accessToken"))
+      orders = orders.filter(value => value.status.includes(filter))
       setOrders(orders);
       // console.log(results[0])
     }
     ordersFetch()
-    
+
     // console.log("results", results)
-  }, [navigate]);
+  }, [filter]);
 
   async function updateDatabaseKitchen(e) {
     // setResults(false)
@@ -57,75 +58,92 @@ function Kitchen() {
     if (e['status'] === 'processed') {
       await database(`orders/${e.id}`, 'PATCH', localStorage.getItem("accessToken"), body2)
       // console.log("delivered")
-    }    
-    orders = await database('orders', 'GET', localStorage.getItem("accessToken"))    
+    }
+    orders = await database('orders', 'GET', localStorage.getItem("accessToken"))
     setOrders(orders)
     return orders
   }
 
   return (
-    <main className="kitchenScreen">
-      {orders && orders.map((e/* , index */) => { // renders products
-        // readyButtonCall(e['status'])
-        // console.log(e.id)
-        return (
-          // results && results.map((e, index) => (
-          <section key={`kitchenScreenSection${e['id']}`}  className="kitchenBox">
-            <p
-              id="textoCorreoInvalido"
-              className="textoCorreoInvalido">
-            STATUS: {e['status']}<br></br><br></br></p>
+    <>
+      <section className="header">
 
-            {e['products'].map((products/* , index */) => {
+        <div className="leftHeader">
+          <li onClick={() => { setFilter('') }}>Todo</li>
+          <li onClick={() => { setFilter('pending') }}>Pendientes</li>
+          <li onClick={() => { setFilter('processed') }}>Procesado</li>
+          <li onClick={() => { setFilter('delivered') }}>Entregado</li>
+        </div>
 
-              // console.log(products['product']['name'])
-              return (
-                <>
-                  {/* <img src={products['product']['image']} alt={products['product']['name']}></img> */}
-                  <p
-                    id="textoCorreoInvalido"
-                    className="textoCorreoInvalido"
-                  >{products['product']['name']} x{products['qty']}</p>
-                </>
-              )
-            })}<br></br>
+        <div className="rightHeader">
+          <li onClick={() => { logout() }}>Logout</li>
+        </div>
 
-            {/* <img src={e['image']} alt={e['name']}></img> */}
-            <p
-              id="textoCorreoInvalido"
-              className="textoCorreoInvalido">
-              LOGGED: {e['dataEntry']}<br></br>
-              {/* {readyText && <p
+      </section>
+
+      <main className="kitchenScreen">
+        {orders && orders.map((e/* , index */) => { // renders products
+          // readyButtonCall(e['status'])
+          // console.log(e.id)
+          return (
+            // results && results.map((e, index) => (
+            <section key={`kitchenScreenSection${e['id']}`} className="kitchenBox">
+              <p
+                id="textoCorreoInvalido"
+                className="textoCorreoInvalido">
+                STATUS: {e['status']}<br></br><br></br></p>
+
+              {e['products'].map((products/* , index */) => {
+
+                // console.log(products['product']['name'])
+                return (
+                  <>
+                    {/* <img src={products['product']['image']} alt={products['product']['name']}></img> */}
+                    <p
+                      id="textoCorreoInvalido"
+                      className="textoCorreoInvalido"
+                    >{products['product']['name']} x{products['qty']}</p>
+                  </>
+                )
+              })}<br></br>
+
+              {/* <img src={e['image']} alt={e['name']}></img> */}
+              <p
+                id="textoCorreoInvalido"
+                className="textoCorreoInvalido">
+                LOGGED: {e['dataEntry']}<br></br>
+                {/* {readyText && <p
                 style={{ visibility: readyText ? 'visible' : 'hidden' }}
               >READY: {e['dateProcessed']}<br></br></p>} */}
-              PROCESSED: {/* {noText && 'No'} */}{e['dateProcessed']}{/* {(e) => {readyText ? `${e['dateProcessed']}` : 'No'}} */}<br></br>
-              DELIVERED: {e['dateDelivered']}</p><br></br>
-            {/* <p id="textoCorreoInvalido" className="textoCorreoInvalido">STATUS: {e['status']}</p> */}
-            <div className="amountBox">
-              {/* <p id={index} onClick={() => { setCounter(counter - 1); console.log(index) }}>{'<'}</p>
+                PROCESSED: {/* {noText && 'No'} */}{e['dateProcessed']}{/* {(e) => {readyText ? `${e['dateProcessed']}` : 'No'}} */}<br></br>
+                DELIVERED: {e['dateDelivered']}</p><br></br>
+              {/* <p id="textoCorreoInvalido" className="textoCorreoInvalido">STATUS: {e['status']}</p> */}
+              <div className="amountBox">
+                {/* <p id={index} onClick={() => { setCounter(counter - 1); console.log(index) }}>{'<'}</p>
             <p id={`counter${index}`}>{counter}</p> */}
 
-              {/* readyButton(e) */}
+                {/* readyButton(e) */}
 
-              <button
-                // data-testid={`buttonid${e['id']}`}
-                // onload={readyButtonCall(e['status'])}
-                onClick={async () => {
-                  await updateDatabaseKitchen(e)
-                  // database(`orders/${e.id}`, 'PATCH', localStorage.getItem("accessToken"), body)
-                  // resultsFetch()
-                  // Kitchen()
-                  // setResults(results)
-                  // window.location.reload(false)
-                }}
-                className="checkoutBoxButtons"
-              >{readyButtonText(e['status'])}</button>
+                <button
+                  // data-testid={`buttonid${e['id']}`}
+                  // onload={readyButtonCall(e['status'])}
+                  onClick={async () => {
+                    await updateDatabaseKitchen(e)
+                    // database(`orders/${e.id}`, 'PATCH', localStorage.getItem("accessToken"), body)
+                    // resultsFetch()
+                    // Kitchen()
+                    // setResults(results)
+                    // window.location.reload(false)
+                  }}
+                  className="checkoutBoxButtons"
+                >{readyButtonText(e['status'])}</button>
 
-            </div>
-          </section>
-        )
-      })}
-    </main>
+              </div>
+            </section>
+          )
+        })}
+      </main>
+    </>
   )
 }
 
--- src/modules/office/components/officeHeader.jsx
+++ src/modules/office/components/officeHeader.jsx
@@ -6,14 +6,21 @@ import { Link/* , useMatch, useResolvedPath */ } from "react-router-dom"
 // import { getElementError } from '@testing-library/react'
 // import ReactDOM from "react-dom"
 // import Workers from './workers';
+import { logout } from "../../../features/logout";
 
 function Header() {
 
   return (
     <main className="header">
 
-      <Link to="/officeWorkers">Office</Link>
-      <Link to="/officeProducts">Products</Link>
+      <div className="leftHeader">
+        <Link to="/officeWorkers">Office</Link>
+        <Link to="/officeProducts">Products</Link>
+      </div>
+
+      <div className="rightHeader">
+        <li onClick={() => { logout() }}>Logout</li>
+      </div>
 
     </main>
   )
